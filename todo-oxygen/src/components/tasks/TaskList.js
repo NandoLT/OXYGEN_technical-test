c6449c8611f21194046cb90a14d77b8bff61da99
Fernando Lopez Trejo <fernando_lt@hotmail.es>
2022-02-18 01:26:22 +0100

    tasklist configuration and structure in task + css

--- todo-oxygen/src/components/tasks/TaskList.js
+++ todo-oxygen/src/components/tasks/TaskList.js
@@ -1,7 +1,23 @@
 import React from 'react'
 
-export default function TaskList() {
-  return (
-    <div>Componente Lsitas</div>
-  )
+import '../../assets/css/taskList.css';
+
+export default function TaskList({ title, goto }) {
+    return (
+        <div className="card list-card">
+            <header className="card-header">
+                <p className="card-header-title">
+                    {title}
+                </p>
+            </header>
+            <div className="card-content">
+                <div className="content">
+                Lorem ipsum leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis consectetur purus sit amet fermentum.
+                </div>
+            </div>
+            <footer className="card-footer">
+                <button class="button is-success">{goto}</button>
+            </footer>
+        </div>
+    )
 }
--- todo-oxygen/src/components/tasks/Tasks.js
+++ todo-oxygen/src/components/tasks/Tasks.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Layout from '../commons/layout/Layout';
 import TaskList from './TaskList';
 
+import '../../assets/css/tasks.css';
+
 export default function Tasks() {
     return (
         <Layout>
-            <TaskList />
+            <div className="lists-container">
+                <TaskList title={'TO-DO'} goto={'done'} />
+                <TaskList title={'DONE'} goto={'todo'}/>
+            </div>
         </Layout>
     )
 }
